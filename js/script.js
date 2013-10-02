a5ce0e33662dacc4f4d2e394de49cb8d10e1cc41
Jessica <missyjcat@gmail.com>
2013-10-01 23:15:31 -0600

    made footer text smaller in mobile, LI bullets left aligned in mobile, fixed email signup in mobile to be responsive

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@
 		});
 
 		addEvent(window, 'resize', function() {
-			// Define wWidth and wHeight on scroll
+			// Define wWidth and wHeight on resize
 			wWidth = $(window).width();
 			wHeight = $(window).height();
 		});
@@ -91,18 +91,6 @@
 		// When signup button is clicked, pop up overlay form.
 		$('.signup-button').click( function() {
 			booksILove.signUpPopup();
-		});
-
-		// When menu button is clicked, slide down/up mobile menu
-		// Then, add class "mobile-menu" to UL to distinguish it from desktop site
-		$('.mobile-menu img').click( function() {
-			$('ul.menu').slideToggle('slow').addClass('mobile-menu');
-		});
-
-		// When menu item is tapped, hide menu
-		// Then, remove "mobile-menu" class
-		$('ul.menu li a').click( function() {
-			$('ul.mobile-menu').hide().removeClass('mobile-menu');
 		});
 
 	};
@@ -125,20 +113,16 @@
 
 
 	booksILove.signUpPopup = function() {
-		// Prepend overlay div and email form and close box
-		$('body').prepend('<div style="-ms-filter:\'progid:DXImageTransform.Microsoft.Alpha(Opacity=80)\';filter: alpha(opacity=80);-khtml-opacity: 0.80;-moz-opacity: 0.80;opacity: 0.80;width:' + wWidth + 'px;height:' + wHeight + 'px;background-color: #ffffff;position:fixed;z-index:200;display: none;" class="overlay"></div><iframe src="http://booksilove.com/stage/content/sign-form" frameborder="0" style="padding:20px;width:450px;height:345px;overflow:hidden;border: 1px solid #999999;z-index: 201;display:none;position: fixed; background-color:#ffffff;left:' + (wWidth/2-225) + 'px;top:' + (wHeight/2-150) + 'px;" class="signup-form"></iframe><div class="closebox" style="position: fixed;top:' + (wHeight/2-140) + 'px;left:' + (wWidth/2+245) + 'px;z-index:201;cursor:pointer;">X</div>');
-
-		// Fade in overlay div and email form
-		$('.overlay').fadeIn('fast');
-		$('.signup-form').fadeIn('slow');
-
-		// Attach click handler to close these things
-		$('.closebox, .overlay').click( function() {
-			$('.overlay, .signup-form, .closebox').fadeOut('slow').remove();
-		});
-
 		
-		addEvent(window, 'resize', function() {
+		if (wWidth > 320) {
+			// Prepend overlay div and email form and close box
+			$('body').prepend('<div style="-ms-filter:\'progid:DXImageTransform.Microsoft.Alpha(Opacity=80)\';filter: alpha(opacity=80);-khtml-opacity: 0.80;-moz-opacity: 0.80;opacity: 0.80;width:' + wWidth + 'px;height:' + wHeight + 'px;background-color: #ffffff;position:fixed;z-index:200;display: none;" class="overlay"></div><iframe src="http://booksilove.com/stage/content/sign-form" frameborder="0" style="padding:20px;width:450px;height:345px;overflow:hidden;border: 1px solid #999999;z-index: 201;display:none;position: fixed; background-color:#ffffff;left:' + (wWidth/2-225) + 'px;top:' + (wHeight/2-150) + 'px;" class="signup-form"></iframe><div class="closebox" style="position: fixed;top:' + (wHeight/2-140) + 'px;left:' + (wWidth/2+245) + 'px;z-index:201;cursor:pointer;">X</div>');
+
+			// Fade in overlay div and email form
+			$('.overlay').fadeIn('fast');
+			$('.signup-form').fadeIn('slow');
+
+			addEvent(window, 'resize', function() {
 			// Update width and height of overlay on resize
 			$('.overlay').css({
 				width: wWidth + 'px',
@@ -157,6 +141,24 @@
 			});
 
 		});
+		}
+		else {
+			// Prepend overlay div and email form and close box -- mobile version
+			$('body').prepend('<div style="background-color: #ffffff;width:' + wWidth + 'px;height:' + wHeight + 'px;min-height: 1600px;background-color: #ffffff;position:fixed;z-index:200;display: none;" class="overlay"></div><iframe src="http://booksilove.com/stage/content/sign-form" frameborder="0" style="padding:0;width:100%;height:345px;overflow:hidden;border:0;z-index: 201;display:none;position: fixed; background-color:#ffffff;left:0;top:100px;" class="signup-form"></iframe><div class="closebox" style="position: fixed;top:10px;right:10px;z-index:201;cursor:pointer;font-size: 2em;font-weight: 600;">X</div>');
+
+			// Fade in overlay div and email form
+			$('.overlay').fadeIn('fast');
+			$('.signup-form').fadeIn('slow');
+
+			console.log('executing this css');
+		}
+
+		// Attach click handler to close these things
+		$('.closebox, .overlay').click( function() {
+			$('.overlay, .signup-form, .closebox').fadeOut('slow').remove();
+		});
+
+		
 	};
 
 	booksILove.initialize();
