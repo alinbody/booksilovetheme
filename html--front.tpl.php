a2d4d62881999bd9c95202295e1f3f97fc14f462
Jessica <missyjcat@gmail.com>
2013-10-02 00:05:31 -0600

    added google analytics tracking

--- html--front.tpl.php
+++ html--front.tpl.php
@@ -52,6 +52,19 @@
 	booksILove.autoScroll('faq');
 	booksILove.autoScroll('contact');
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38529879-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <?php print $page_bottom; //stuff from modules always render last ?>
 </body>
 </html>
